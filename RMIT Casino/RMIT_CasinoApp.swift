dada957806eef7d2b8017dbd7c51e6488edef35e
Asforq <s3878010@rmit.edu.vn>
2024-08-08 13:48:20 +0700

    Initial Commit

---
+++ RMIT Casino/RMIT_CasinoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RMIT_CasinoApp.swift
+//  RMIT Casino
+//
+//  Created by Anna Phan on 8/8/24.
+//
+
+import SwiftUI
+
+@main
+struct RMIT_CasinoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
